1036b4c2dbcb87610ed3f92218aba396a3035ad7
Vasylyna Bizniakova <vbiznyakova@gmail.com>
2026-04-19 23:58:43 +0300

    fix timer styles, add timer scripts

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -5,42 +5,101 @@ import 'flatpickr/dist/flatpickr.min.css';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const input = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
 
-const options = {
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+let userSelectedDate = null;
+let timerId = null;
+
+startBtn.disabled = true;
+
+flatpickr(input, {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    const selectedDate = selectedDates[0];
+    const currentDate = new Date();
+
+    if (selectedDate <= currentDate) {
+      startBtn.disabled = true;
+      userSelectedDate = null;
+
+      iziToast.error({
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+        backgroundColor: '#ef4040',
+        messageColor: '#ffffff',
+        progressBarColor: '#b51b1b',
+        close: true,
+        icon: '',
+        timeout: 3000,
+        class: 'custom-toast',
+      });
+
+      return;
+    }
+
+    userSelectedDate = selectedDate;
+    startBtn.disabled = false;
   },
-};
+});
 
-let userSelectedDate = null;
+startBtn.addEventListener('click', () => {
+  if (!userSelectedDate) return;
 
-flatpickr(input, options);
+  clearInterval(timerId);
+
+  startBtn.disabled = true;
+  input.disabled = true;
+
+  updateTimer();
+  timerId = setInterval(updateTimer, 1000);
+});
+
+function updateTimer() {
+  const currentTime = new Date();
+  const deltaTime = userSelectedDate - currentTime;
+
+  if (deltaTime <= 0) {
+    clearInterval(timerId);
+    updateTimerFace({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+
+    input.disabled = false;
+    startBtn.disabled = true;
+    return;
+  }
+
+  const time = convertMs(deltaTime);
+  updateTimerFace(time);
+}
+
+function updateTimerFace({ days, hours, minutes, seconds }) {
+  daysEl.textContent = addLeadingZero(days);
+  hoursEl.textContent = addLeadingZero(hours);
+  minutesEl.textContent = addLeadingZero(minutes);
+  secondsEl.textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
-console.log('Timer code');
