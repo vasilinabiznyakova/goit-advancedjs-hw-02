9c490e828d5d9c81f8634a7a84388aea5982e9c6
Vasylyna Bizniakova <vbiznyakova@gmail.com>
2026-04-20 00:10:10 +0300

    add script for snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,51 @@
-console.log('Snackbar code');
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const delay = Number(formData.get('delay'));
+  const state = formData.get('state');
+
+  createPromise(delay, state)
+    .then(delayValue => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delayValue}ms`,
+        position: 'topRight',
+        close: false,
+        icon: '',
+
+        backgroundColor: '#4caf50',
+        messageColor: '#ffffff',
+        progressBarColor: '#2e7d32',
+      });
+    })
+    .catch(delayValue => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delayValue}ms`,
+        position: 'topRight',
+        close: false,
+        icon: '',
+        backgroundColor: '#ef4040',
+        messageColor: '#ffffff',
+        progressBarColor: '#b51b1b',
+      });
+    });
+
+  form.reset();
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
